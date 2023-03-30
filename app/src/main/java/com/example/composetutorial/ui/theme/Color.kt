a6f48c055e3b4af585732ac582dbc7679cd87edb
karinamartinssoft <karina.martins@softniels.com.br>
2023-03-30 16:32:28 -0300

    Initial commit

---
+++ app/src/main/java/com/example/composetutorial/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.composetutorial.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFF5F34C4)
+val PurpleGrey80 = Color(0xFFBAA4DD)
+val Pink80 = Color(0xFFFFC1D3)
+
+val Purple40 = Color(0xFF5F1D8F)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
